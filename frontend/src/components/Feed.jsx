8bbf7af09746b3b9e9d68982684a6afb8442becb
Tarunksaini <tarunshr145@gmail.com>
2024-09-29 00:56:20 +0530

    ready for production

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -116,10 +116,10 @@ const Feed = () => {
                   <CiLocationOn size="24px" />
                 </div>
               </div>
-
-              <button
-                className="bg-black text-white rounded-full p-2 text-sm w-[60px] flex items-center justify-center absolute right-2  hover:bg-blue"
-                onClick={handlePost}>
+              <button className="flex justify-center items-center  bg-[#0866FF] rounded-[1.25em] border-0 transition-all duration-200 hover:shadow-[3.4px_2.5px_4.9px_rgba(0,0,0,0.025),_8.6px_6.3px_12.4px_rgba(0,0,0,0.035),_17.5px_12.8px_25.3px_rgba(0,0,0,0.045),_36.1px_26.3px_52.2px_rgba(0,0,0,0.055),_99px_72px_143px_rgba(0,0,0,0.08)] p-2 text-sm  absolute right-2" onClick={handlePost}>
+                <svg className="svgIcon w-5 h-5 mr-2" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path>
+                </svg>
                 Post
               </button>
             </div>
